523626645891000950d2ff31ef6b5c209d23ade7
Reymund Panaguiton <Reymund@reymundos-imac.local>
2018-10-01 18:50:48 +0800

    commit table view cell

--- WeightAndLevel/Controller/TableViewCell.swift
+++ WeightAndLevel/Controller/TableViewCell.swift
@@ -19,6 +19,7 @@ class TableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
